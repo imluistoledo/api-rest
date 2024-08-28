a547439f222bec652d0b8168a51d7fc318e67f2a
imluistoledo <imluistoledo@gmail.com>
2024-08-28 12:54:05 -0500

    idioma y secciones

--- primer-parcial/02-consulta-api/fetch.js
+++ primer-parcial/02-consulta-api/fetch.js
@@ -1,4 +1,5 @@
 let pagina = 1
+let idioma = ''
 const btnAnterior = document.getElementById('btnAnterior')
 const btnSiguiente = document.getElementById('btnSiguiente')
 
@@ -16,18 +17,25 @@ btnAnterior.addEventListener('click', () => {
 	}
 })
 
+btnCargarPeliculas.addEventListener('click', () => {
+    idioma = document.getElementById('idiomas').value
+    pagina = document.getElementById('secciones').value
+    cargarPeliculas()
+})
+
 
 const cargarPeliculas = () => {
     let peliculas = ''
-    const peticion = fetch(`https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=${pagina}`)
+    const peticion = fetch(`https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=${idioma}&page=${pagina}`)
         .then(respuesta => respuesta.json())
         .then(peliculas => {
             peliculas.results.forEach(pelicula => {
                 peliculas += `
                 <div class="pelicula">
-                <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+                <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.backdrop_path}">
                 <h3 class="titulo">${pelicula.title}</h3>
                 </div>
+                <p class="descripcion">${pelicula.overview}</p>
 				`
             })
             peliculas = peliculas.replace('[object Object]', '')
@@ -37,4 +45,4 @@ const cargarPeliculas = () => {
 
 }
 
-window.onload = cargarPeliculas
+//window.onload = cargarPeliculas
