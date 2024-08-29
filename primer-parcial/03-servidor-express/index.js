9799e33855f01375244ea6c804046ee37b5c44f0
imluistoledo <imluistoledo@gmail.com>
2024-08-29 12:07:25 -0500

    servidor express

---
+++ primer-parcial/03-servidor-express/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors'); // Importar cors
+const app = express();
+const port = 3000;
+
+// Habilitar CORS para todas las rutas
+app.use(cors());  
+
+app.get('/', (req, res) => {
+  res.send('¡Hola Mundo!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
