52eae959ab7aaa1914de9a60501c10a084ed6f81
imluistoledo <imluistoledo@gmail.com>
2024-09-06 09:46:17 -0500

    update datos

--- primer-parcial/06-recibir-datos/index.js
+++ primer-parcial/06-recibir-datos/index.js
@@ -2,26 +2,26 @@
 // get param ruta
 // post cadena consulta
 // put body json
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const port = 3000
 
 app.use(express.json())
 
-app.listen(3000, () => {
-    console.log(`Servidor escuchando en http://localhost:${port}`);
-});
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`)
+})
 
 app.get('/user/:name', (req, res) => {
-    const username = req.params.name;
-    res.send(`Nombre del usuario: ${username}`);
-});
+    const username = req.params.name
+    res.send(`Nombre del usuario: ${username}`)
+})
 
 app.post('/cadena-consulta', (req, res) => {
-    const nombre = req.query.nombre;
-    res.send(`Hola, ${nombre}!`);
-});
+    const nombre = req.query.nombre
+    res.send(`Hola, ${nombre}!`)
+})
 
 app.put('/body-json', (req, res) => {
     console.log(req.body)
-});
+})
