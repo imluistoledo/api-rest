1b48f5cec4004751641583c131908032930e2d2d
imluistoledo <imluistoledo@gmail.com>
2024-10-10 19:07:18 -0500

    modulos ecma script

---
+++ segundo-parcial/05-documentacion-esdoc/funciones.js
@@ -0,0 +1,32 @@
+/**
+ * Calcula el cuadrado de un número.
+ * @param {number} number - El número a ser elevado al cuadrado.
+ * @returns {number} El cuadrado del número dado.
+ */
+export const square = (number) => {
+    return number === 0 ? 1 : number * number
+}
+
+/**
+ * Calcula el cubo de un número.
+ * @param {number} number - El número a ser elevado al cubo.
+ * @returns {number} El cubo del número dado.
+ */
+export const cube = (number) => {
+    return number === 0 ? 0 : number ** 3
+}
+
+/**
+ * Calcula el area de un circulo.
+ * @param {number} radius - El radio del circulo.
+ * @returns {number} El area de un circulo con el radio pasado.
+ */
+export const circle = (radius) => {
+    return Math.pi * (radius**2)
+}
+
+// module.exports = {
+//     square,
+//     cube,
+//     circle
+// }
