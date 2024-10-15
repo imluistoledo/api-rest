49bc95537420d64d908119f1e15f4ef0281d0491
imluistoledo <imluistoledo@gmail.com>
2024-10-15 09:54:36 -0500

    sanitizacion de la entrada

--- segundo-parcial/06-express-validator/reglasValidacion.js
+++ segundo-parcial/06-express-validator/reglasValidacion.js
@@ -1,12 +1,25 @@
-// validationRules.js
 const { body } = require('express-validator')
 
-// Definir reglas de validación
+// Definir reglas de validación y sanitización
 const registerValidationRules = () => {
     return [
-        body('username').isLength({ min: 5 }).isAlphanumeric().withMessage('El nombre de usuario debe tener al menos 5 caracteres y ser alfanumérico'),
-        body('email').isEmail().withMessage('Debe proporcionar un correo electrónico válido'),
-        body('password').isLength({ min: 8 }).withMessage('La contraseña debe tener al menos 8 caracteres')
+        // Validación del nombre de usuario
+        body('username')
+            .trim() // Eliminar espacios en blanco al principio y al final
+            .escape() // Escapar caracteres especiales como <, >, &, etc.
+            .isLength({ min: 5 }).withMessage('El nombre de usuario debe tener al menos 5 caracteres'),
+
+        // Validación del email
+        body('email')
+            .trim()
+            .normalizeEmail() // Normalizar el correo (por ejemplo, convertir todo a minúsculas)
+            .isEmail().withMessage('Debe proporcionar un correo electrónico válido'),
+
+        // Validación de la contraseña
+        body('password')
+            .trim() // Eliminar espacios en blanco al principio y al final
+            .escape() // Escapar caracteres especiales para prevenir inyecciones
+            .isLength({ min: 8 }).withMessage('La contraseña debe tener al menos 8 caracteres')
     ]
 }
 
