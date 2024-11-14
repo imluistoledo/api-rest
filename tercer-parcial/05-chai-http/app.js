2b2d59a1e7ac9b3dc3237e160f4d50f59649bb93
imluistoledo <imluistoledo@gmail.com>
2024-11-14 09:24:58 -0600

    limpieza en comentarios

--- tercer-parcial/05-chai-http/app.js
+++ tercer-parcial/05-chai-http/app.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 app.get('/users', (req, res) => {
-    res.json([{ id: 1, name: 'John Doe' }, { id: 2, name: 'Jane Doe' }]);
-});
+    res.json([{ id: 1, name: 'John Doe' }, { id: 2, name: 'Jane Doe' }])
+})
 
-module.exports = app;
+module.exports = app
--- tercer-parcial/05-chai-http/test/app.test.js
+++ tercer-parcial/05-chai-http/test/app.test.js
@@ -1,22 +1,22 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('../app'); // Importamos la aplicación de Express
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const app = require('../app')
 
-const { expect } = chai;
-chai.use(chaiHttp);
+const { expect } = chai
+chai.use(chaiHttp)
 
 describe('API Users', () => {
     // Prueba para la ruta GET /users
     it('debería obtener una lista de usuarios', (done) => {
         chai
             .request(app)
-            .get('/users') // Ruta que queremos probar
+            .get('/users')
             .end((err, res) => {
-                expect(res).to.have.status(200); // Comprobamos que el estado sea 200
-                expect(res.body).to.be.an('array'); // Comprobamos que el cuerpo sea un array
-                expect(res.body.length).to.be.equal(2); // Comprobamos que tenga 2 usuarios
-                expect(res.body[0]).to.have.property('name'); // Comprobamos que cada usuario tenga el campo 'name'
-                done();
-            });
-    });
-});
+                expect(res).to.have.status(200)
+                expect(res.body).to.be.an('array')
+                expect(res.body.length).to.be.equal(2)
+                expect(res.body[0]).to.have.property('name')
+                done()
+            })
+    })
+})
