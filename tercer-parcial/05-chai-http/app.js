c4ec742cf6962f1b12ef399c5d9171f92f44ec24
imluistoledo <imluistoledo@gmail.com>
2024-11-14 00:46:24 -0600

    server con commonjs

---
+++ tercer-parcial/05-chai-http/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/users', (req, res) => {
+    res.json([{ id: 1, name: 'John Doe' }, { id: 2, name: 'Jane Doe' }]);
+});
+
+module.exports = app;
